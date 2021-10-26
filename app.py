7b50ed1f464a7e7dfdf0f7260d349d6488f547c3
wimedina <wimedina93@gmail.com>
2021-10-25 23:56:15 -0500

    Tercer commit dashboard administrador añadido

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template, request, flash
 from werkzeug.utils import redirect
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
 
-@app.route('/menu', methods=['GET','POST'])
+
+@app.route('/menu', methods=['GET', 'POST'])
 def menu():
     if request.method == 'GET':
         return render_template('menuPlatos.html')
 
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html')
